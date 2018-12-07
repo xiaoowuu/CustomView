1c270d3ccc2ce09c6a1a233f64f9a8aad8c92722
xiaoowuu <xiaoowuu@gmail.com>
2018-12-07 10:50:12 +0800

    [dev]心电图注释

--- ecgview/src/main/java/win/smartown/library/ecgview/EcgUtil.java
+++ ecgview/src/main/java/win/smartown/library/ecgview/EcgUtil.java
@@ -31,37 +31,79 @@ public class EcgUtil {
         paint.setStyle(Paint.Style.STROKE);
     }
 
+    /**
+     * 设置波线颜色
+     *
+     * @param lineColor 波线颜色
+     * @return this
+     */
     public EcgUtil setLineColor(int lineColor) {
         paint.setColor(lineColor);
         return this;
     }
 
+    /**
+     * 设置波线宽度
+     *
+     * @param lineWidth 波线宽度
+     * @return this
+     */
     public EcgUtil setLineWidth(int lineWidth) {
         this.lineWidth = lineWidth;
         paint.setStrokeWidth(lineWidth);
         return this;
     }
 
+    /**
+     * 设置拐角半径
+     *
+     * @param cornerRadius 拐角半径
+     * @return this
+     */
     public EcgUtil setCornerRadius(int cornerRadius) {
         paint.setPathEffect(new CornerPathEffect(cornerRadius));
         return this;
     }
 
+    /**
+     * 设置图片高度
+     *
+     * @param height 图片高度
+     * @return this
+     */
     public EcgUtil setHeight(int height) {
         this.height = height;
         return this;
     }
 
+    /**
+     * 设置波峰值
+     *
+     * @param maxValue 波峰值
+     * @return this
+     */
     public EcgUtil setMaxValue(float maxValue) {
         this.maxValue = maxValue;
         return this;
     }
 
+    /**
+     * 设置两个数据间间隔宽度
+     *
+     * @param dividerWidth 两个数据间间隔宽度
+     * @return this
+     */
     public EcgUtil setDividerWidth(int dividerWidth) {
         this.dividerWidth = dividerWidth;
         return this;
     }
 
+    /**
+     * 生成心电图图片
+     *
+     * @param values 心电图数据
+     * @return 心电图图片
+     */
     @Nullable
     public Bitmap getResult(float... values) {
         if (values == null || values.length == 0) {
